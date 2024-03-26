a0874324686273492645f8be1bdce1230947b4df
Steven Sierra <stevensierra.dev@gmail.com>
2024-03-26 04:39:40 -0500

    fix(prettier): fix prettier layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <QueryProvider>
-          <UIProvider>
-          {children}
-          </UIProvider>
+          <UIProvider>{children}</UIProvider>
         </QueryProvider>
       </body>
     </html>
